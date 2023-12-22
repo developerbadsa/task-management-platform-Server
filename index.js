a2cc234dc778fd05b02e4a44dded6c41982b6791
Rahim Badsa <developerbadsa@gmail.com>
2023-12-22 15:37:16 +0600

    created route for update task status

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const {MongoClient, ObjectId} = require('mongodb');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = 3000;
 require('dotenv').config();
-
-
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,37 +13,65 @@ const client = new MongoClient(uri);
 
 async function run() {
   try {
-//     const TasksDB = client.db('taskDB').collection('taskDBCollection');
-
-
+    //     const TasksDB = client.db('taskDB').collection('taskDBCollection');
 
     app.get('/', (req, res) => {
-      // res.send('');
+      res.send('go');
     });
-    
+
+    //---------------------GET REQ -------------------------//
+    app.get('/tasks', async (req, res) => {
+      // const {email, status} = req.body
+      const {status, email} = req.query;
+      const query = {status: status};
+
+      const result = await client
+        .db('taskDB')
+        .collection(email)
+        .find(query)
+        .toArray();
+      // console.log('task', result, status);
+      res.send(result);
+    });
+
     //---------------------POST REQ -------------------------//
     app.post('/create-task', async (req, res) => {
-      const {formData, email} = req.body
-          
-    const result = await client.db('taskDB').collection(email).insertOne(formData);
+      const {formData, email, status} = req.body;
+
+      const result = await client
+        .db('taskDB')
+        .collection(email)
+        .insertOne({formData, status});
       console.log(result);
-      res.send(result)
+      res.send(result);
     });
 
+    //---------------------UPDATE REQ -------------------------//
+    app.put('/task-status', async (req, res) => {
+      const {shouldgo, email, id} = req?.query;
 
+      const updateDoc = {
+        $set: {
+          status: shouldgo,
+        },
+      };
+      const query = {
+            _id: new ObjectId(id)
+      }
 
-
-
-
+      const result = await client
+        .db('taskDB')
+        .collection(email)
+        .updateOne(query, updateDoc);
+      res.send(result)
+    });
   } finally {
     // Close the MongoDB client connection
-//     await client.close();
+    //     await client.close();
   }
 }
 // Run the function and handle any errors
 run().catch(console.dir);
-
-
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
